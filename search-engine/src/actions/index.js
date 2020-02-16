952e41c57bff04d5f83bc8e786846f14d9406305
subhadip0608 <subhadip0608@gmail.com>
2020-02-16 22:33:29 +0100

    removed unused code

--- search-engine/src/actions/index.js
+++ search-engine/src/actions/index.js
@@ -2,9 +2,6 @@ import { PERFORM_SEARCH } from "./types";
 import searchBase from "../api/searchBase";
 
 export const performSearch = searchKey => async dispatch => {
-  var params = new URLSearchParams();
-  params.append("indexName", "ikea");
-  params.append("params", `query=${searchKey}&hitsPerPage=16`);
   var response = await searchBase.post("", {
     requests: [
       {
